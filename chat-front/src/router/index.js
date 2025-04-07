b62fd5b9ee891e36d225f1f7a5c4c5987f87dd7f
limdodod <rosalim2001@naver.com>
2025-04-07 20:33:17 +0900

    redis-front

--- chat-front/src/router/index.js
+++ chat-front/src/router/index.js
@@ -6,6 +6,7 @@ import MemberList from "@/views/MemberList.vue"
 import SimpleWebsocket from "@/views/SimpleWebsocket.vue"
 import StompChatPage from "@/views/StompChatPage.vue"
 import GroupChattingList from "@/views/GroupChattingList.vue"
+import MyChatPage from "@/views/MyChatPage.vue"
 
 const routes = [
  {
@@ -37,6 +38,11 @@ const routes = [
    path:'/chatpage/:roomId',
    name: 'StompChatPage',
    component: StompChatPage
+},
+{
+   path:'/my/chat/page',
+   name: 'MyChatPage',
+   component: MyChatPage
 }
 
 ]
